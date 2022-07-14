2131985c85d1c80657e97f9bccca74ab72b684b8
Yuri Campos <yurinapoleao@gmail.com>
2022-07-13 21:02:06 -0300

    Starting the header component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,14 @@
 const Header = () => {
-	return <h1>Header</h1>;
+	return (
+		<>
+			<header
+				className="flex items-center justify-between p-5 max-w-7xl m-auto
+				sm:py-10 sm:px-5"
+			>
+				Header
+			</header>
+		</>
+	);
 };
 
 export default Header;
